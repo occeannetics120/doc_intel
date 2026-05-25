ae4f3e66f2fc52e4cb8b889fc21e42f6d0d2536e
occeannetics120 <oceannetics@gmail.com>
2026-05-25 16:29:00 +0530

    sending question and the context separately

--- src/application/llm/query_llm.rs
+++ src/application/llm/query_llm.rs
@@ -5,17 +5,24 @@ use crate::api::post_main_query::llmInput;
 pub async fn query_llm(input: llmInput) {
     let client = reqwest::Client::new();
 
-    let mut input_concated = input.question;
+    let mut questions = input.question;
 
     let chunks_concated = input.chunks.join("\n\n");
 
-    input_concated = input_concated + &chunks_concated;
+    let mut context_concated = "The following is the context to answer the question , if the exact answer isnt there , say that you couldnt give the answer  : ".to_string();
+
+    context_concated = context_concated + &chunks_concated;//we had to convert context to_string cause the former is a static and we ahve to make it dyanmic whic in turn allocateds it on heap and gives us the ownership .. 
+
 
     let llm_resp = client
         .post("http://localhost:11434/api/chat")
         .json(&serde_json::json!({
                 "model":"qwen2.5:7b",
-                "messages": [ {"role":"user","content":input_concated}],
+                "messages": [ 
+                    {"role" : "system" , "content": context_concated},
+                    {"role":"user","content":questions},
+                    
+                    ],
                  "stream": false,
         }))
         .send()
